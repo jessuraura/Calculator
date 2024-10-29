74a1fe20af2a1912fa8dc5b27c8161f7236ad49e
moiskieee <moisesleroyendaya215@gmail.com>
2024-10-29 14:48:54 +0800

    anneoyng

--- calculator.py
+++ calculator.py
@@ -3,7 +3,30 @@
 # Subtraction Method
 
 # Multiplication Method
-
+def multiply (x,y):
+    return x * y
 # Division Method
 
 # Main Method
+print("Select operation:")
+print("Press (+) for Addition")
+print("Press (-) for Subtraction")
+print("Press (x) for Multiplication")
+print("Press (/) for Division")
+
+choice = input ("Enter your operation")
+
+num1 = float(input("Enter first number: "))
+num2 = float (input("Enter second number: "))
+
+if choice == '+':
+    print("Result: ", add(num1, num2))
+
+elif choice == '-':
+    print("Result: ", subtract(num1, num2))
+elif choice == 'x':
+    print("Result: ", multiply(num1, num2))
+elif choice == '/':
+    print("Result: ", divide(num1, num2))
+else:
+    print("Invalid input")
